6bcc4ede5b0d05c811babae0a092e98d74dc89a6
Chioma Ozuru <127309670+Chichibekee@users.noreply.github.com>
2025-05-13 17:03:20 +0100

    Add files via upload

---
+++ Methods-Fuctions/script.js
@@ -0,0 +1,70 @@
+//String Methods
+// let myStr = 'Hello, janet world';
+// let myName = "Chioma Ozuru"
+
+// console.log(myStr.toLowerCase()); // hello, janet world
+// console.log(myStr.toUpperCase()); // HELLO, JANET WORLD
+// console.log(myStr.toLowerCase ()); //
+// console.log(myStr.charAt());
+// console.log(myStr.indexOf('Javascript'));
+// console.log(myStr.includes('Javascript'));
+// console.log(myStr.startsWith('Hello'));
+// console.log(myStr.trim());
+// console.log(myStr.split(' '));``
+// console.log(myName.split(' ')); // [ 'Chioma', 'Ozuru' ]
+// console.log(myStr.endsWith)("Hi")
+
+// //Number Methods
+// let num = 56.908
+// let  strNum = "89.56"
+
+// console.log(num);
+// console.log(num.toString());
+// console.log(num.toFixed(2));
+// console.log(parseInt(strNum));
+// console.log(parseFloat(strNum));
+// console.log(isNaN('trtr'));
+
+// // Math Method
+// console.log(Math.PI);
+// console.log(Math.round(41.473));
+// console.log('object');
+// console.log(Math.floor(42.9))
+// console.log(Math.ceil(42.1))
+// console.log(Math.abs(-45.6));
+// console.log(Math.max(64, 23, 64, 23, 78, 90));
+// console.log(Math.min(64, 23, 64, 23, 78, 90));
+
+// console.log(Math.random());
+// console.log(Math.pow(2, 3)); // 2^3 = 8
+// console.log(Math.sqrt(64)); // 8
+// console.log(Math.random() * 100); // Random number between 0 and 100    
+
+// //objects
+
+// let student = {
+//     name: "Chioma",
+//     age: 23,
+//     email: "chommybless93@gmail.com",
+// };
+// console.log(student); 
+// console.log(student.name); 
+// console.log(Object.keys(student)); // [ 'name', 'age', 'email' ]
+// console.log(Object.values(student)); // [ 'Chioma', 23, '
+
+
+// //Fuctions
+// function myName(name) {
+//     console.log(name);
+// }
+
+// myName("Chioma Ozuru"); // Chioma Ozuru
+// myName('Esther Okoro'); // Esther Okoro
+// myName('Janet Okoro'); // Janet Okoro
+
+function addnum(b,a) {
+    console.log(b + a);
+
+    addnum(4,5);
+    addnum(34, 67)
+}
