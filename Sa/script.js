e0e0ecffdb41f3e6e4b1ed7b6bca256974a5f31d
Chioma Ozuru <127309670+Chichibekee@users.noreply.github.com>
2025-05-17 16:19:31 +0100

    Add files via upload

---
+++ Sa/script.js
@@ -0,0 +1,72 @@
+//String Methods
+// let myStr = "Hello, Javascript World!     ";
+// let myName = "Olorunda Samuel";
+
+// console.log(myStr);
+// console.log(myStr.toUpperCase());
+// console.log(myStr.toLowerCase());
+// console.log(myStr.charAt());
+// console.log(myStr.indexOf("Javascript"));
+// console.log(myStr.includes("Hello"));
+// console.log(myStr.slice(2, 8));
+// console.log(myStr.trim());
+// console.log(myStr.replace("World", "Universe"));
+// console.log(myStr.split(" "));
+// console.log(myName.split(" ")[1]);
+// console.log(myStr.startsWith("Hello"));
+// console.log(myStr.endsWith("Hi"));
+// console.log(myName + myStr);
+
+//Number Methods
+// let num = 42.678;
+// let strNum = "89.56";
+
+// console.log(num);
+// console.log(num.toString());
+// console.log(num.toFixed(2));
+// console.log(parseInt(strNum));
+// console.log(parseFloat(strNum));
+// console.log(isNaN("trtr"));
+
+//Math Method
+
+// console.log(Math.PI);
+// console.log(Math.round(41.673));
+// console.log(Math.floor(42.9));
+// console.log(Math.ceil(42.1));
+// console.log(Math.max(12, 6, 7));
+// console.log(Math.min(34, 5, 7));
+// console.log(Math.pow(2, 10));
+// console.log(Math.sqrt(144));
+
+// console.log(Math.random());
+// console.log(Math.random() * 10000);
+
+// console.log(Math.floor(Math.random() * 100));
+
+//Objects
+
+// let student = {
+//   name: "Shade",
+//   age: 34,
+//   email: "shad@gmail.com",
+// };
+
+// console.log(student);
+// console.log(student.name);
+// console.log(Object.keys(student));
+// console.log(Object.values(student));
+
+//Functions
+
+function myName(name) {
+  console.log(name);
+}
+
+myName("Chioma");
+myName("Esther");
+myName("Ruth");
+
+function addNum(a, b) {
+  console.log(a + b);
+}
