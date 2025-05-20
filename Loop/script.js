9e40d26c543fd5bfe889d032b9be3ad68f420f80
Chioma Ozuru <127309670+Chichibekee@users.noreply.github.com>
2025-05-20 17:07:10 +0100

    Add files via upload

---
+++ Loop/script.js
@@ -0,0 +1,39 @@
+// loops is way to write  reapeated functions
+
+// for loop
+// for(initialization; condition; increment/decrement) {
+//     // code to be executed
+// }
+// // Example of a for loop
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+// //  or  
+//  if (i < 5) 
+//     for (let i = 0; i < 5; i++) {
+//         console.log('This is the value of i',i)
+//     }
+//     console.log(' We are laring loops', i)
+
+
+// for(let startingindex = 1; startingindex <= 50; startingindex+= 2) {
+//     if (startingindex % 2 == 0) {
+//         console.log(startingindex, ' is an even number')
+//     }
+//     else {
+//         console.log(startingindex, ' is an odd number')
+//     }
+// }
+
+// // Wrte a fourloop,let the starting value by 5 =< 20
+// for(i = 5; i <= 20; i++) {
+//     console.log('The value of i is', i)
+// }
+
+// Create an array callled it score,
+let score = [10, 20, 30, 40, 50,]
+for(let i = 0; i < 5; i++) {
+    console.log('The value of score is', score[i])
+    let num = 1
+    console.log(num)
+}
