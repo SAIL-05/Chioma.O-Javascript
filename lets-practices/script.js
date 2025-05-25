60b7483f0a0fbafe85bc56ae731caba151a8e030
Chioma Ozuru <127309670+Chichibekee@users.noreply.github.com>
2025-05-25 19:07:44 +0100

    Add files via upload

---
+++ lets-practices/script.js
@@ -0,0 +1,12 @@
+//window.alert('hello chichi');
+
+//let age = prompt('How old are you?');
+//console.log(age);
+
+
+var number = 3;
+console.log(number);
+
+//number = number + 5;
+//console.log(number);
+
