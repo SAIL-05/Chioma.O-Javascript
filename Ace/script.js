49280cd979b89fa08824c1d1088015440d169208
Chioma Ozuru <127309670+Chichibekee@users.noreply.github.com>
2025-05-26 18:48:49 +0100

    Add files via upload

---
+++ Ace/script.js
@@ -0,0 +1,62 @@
+// variable declaration var name;
+// assignment gender = 'female'
+// initialization let name = 'ife' declaration + assignment
+
+// VAR
+// can be redeclared and reassigned
+// LET
+// can be reassigned but not redeclared in the scope
+// CONST
+// can not be reassigned or redeclared
+
+
+// DATA TYPES
+// PRIMITIVE - 7 - string ~ number ~ boolean ~ undefined ~ null ~ bigint ~ symbol
+// NON-PRIMITIVE (OBJECT) - arrays ~ objects etc
+
+// TYPE CONVERSIONS
+// STRING Numbers BOOLEAN
+
+// String() Number() Boolean()
+// toString() parseInt()
+// parseFloat()
+
+// OPERATORS
+// arithmetic logical comparison assignment
+
+// Arithmetic - addition (+) subtraction (-) multiplication (*) division (/) modulus (%)
+
+// Assignment - = += -= *= /= %=
+
+// Comparison - > < == === != !== >= <=
+
+// Logical - AND (&&) OR (||) NOT (!)
+
+// let myNum = document.getElementById("count");
+// let increaseBtn = document.getElementById("btn1");
+// let decreaseBtn = document.getElementById("btn2");
+// let resetBtn = document.getElementById("btn3");
+
+
+// function inc() {
+//     myNum.textContent++;
+// }
+
+// function dec() {
+//     myNum.textContent--;
+// }
+
+// function reset() {
+//     myNum.textContent = 0;
+
+//window.alert('hello chichi');
+
+//let age = prompt('How old are you?');
+//console.log(age);
+
+
+var number = 3;
+console.log(number);
+
+//number = number + 5;
+//console.log(number);
