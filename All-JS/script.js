d353a0f8c407a6bb0682577d4754ff3a346b7bbd
Chioma Ozuru <127309670+Chichibekee@users.noreply.github.com>
2025-06-06 19:46:16 +0100

    Update script.js

--- All-JS/script.js
+++ All-JS/script.js
@@ -919,5 +919,26 @@ async function getAllPost() {
   let blogBlogData = await fetch("https://jsonplaceholder.typicode.com/posts");
   let realBlogData = await blogPosts.json();
   console.log(realBlogData)
-  
+
+const yearOfBirth = document.getElementById('age')
+
+const input = Number(prompt('Date of birth'))
+
+yearOfBirth.textContent = `you are ${(2025 - input)} years old`
+
+const yrOfBirth = document.getElementById('age')
+const ageAns = document.getElementById('ageAns')
+const btn = document.getElementById('btn')
+
+
+function smbBtn (){
+    let answer = Number(yrOfBirth.value)
+    console.log(answer)
+    ageAns.textContent = `you are ${(2025 - answer)} years old` 
+    // console.log(ageAns.textContent = `you are ${(2025 - answer)} years old` )
+}
+
+btn.addEventListener('click', smbBtn)
+
+    
 
