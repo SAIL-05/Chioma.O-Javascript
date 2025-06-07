847ded44cbc1ec651968be1ddd95bf0750e337ce
Chioma Ozuru <127309670+Chichibekee@users.noreply.github.com>
2025-06-07 17:16:04 +0100

    Update script.js

--- All-JS/script.js
+++ All-JS/script.js
@@ -885,7 +885,29 @@ const fetchData = async () => {
 // })      
 // .catch((error) => {
 //     console.error("Error fetching data:", error);
-// });     
+// });    
+
+// console.log(document.getElementById('input-box'))
+
+function AddNewTask() {
+    let inputBox = document.getElementById('input-box');
+    let newlyCreatedElement = document.createElement('li');
+
+
+    newlyCreatedElement.textContent = inputBox.value;
+    document.getElementById('tasklist').appendChild(newlyCreatedElement);
+
+
+}
+
+// function SendNewMessage() {
+//     let userInput = document.getElementById('text-message')
+//     let newChat = document.createElement('p');
+
+//     newChat.textContent = userInput.value;
+//     document.getElementById('chat-app').appendChild(newChat)
+// }
+
 
 
 
