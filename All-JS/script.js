ed24ac7291a388f7dc83876616693a062ca2fcc6
Chioma Ozuru <127309670+Chichibekee@users.noreply.github.com>
2025-06-14 21:08:55 +0100

    Update script.js

--- All-JS/script.js
+++ All-JS/script.js
@@ -1168,14 +1168,69 @@ else {
     alert(`you scored ${grade} is invalid`)
 }
 
+(async function fetchAllMovies() {
+  try {
+    let url =
+      "https://api.themoviedb.org/3/movie/popular?include_adult=false&language=en-US&page=1&api_key=1ef33d0988889fd4f6c374211d20e38c";
+
+    let allMovies = await fetch(url);
+    let movies = await allMovies.json();
+    console.log(movies);
+
+    let movieResults = movies.results;
+    console.log(movieResults);
+
+    console.log(movieResults[0]);
+
+    let movieContainer = document.getElementById("movie-list");
+
+    // DISPLAY SINGLE MOVIE
+
+    // let newDiv = document.createElement("div");
+    // let newImg = document.createElement("img");
+    // newImg.src = `https://image.tmdb.org/t/p/w185${movieResults[3].poster_path}?api_key=1ef33d0988889fd4f6c374211d20e38c`;
+    // newImg.alt = movieResults[3].title;
+
+    // let newText = document.createElement("h3");
+
+    // newText.textContent = movieResults[3].title;
+
+    // newDiv.appendChild(newImg);
+    // newDiv.appendChild(newText);
+
+    // movieContainer.appendChild(newDiv);
+
+
+
+    // DISPLAY ALL MOVIES
+
+    for (let i = 0; i < movieResults.length; i++) {
+      let newDiv = document.createElement("div");
+      let newImg = document.createElement("img");
+      newImg.src = `https://image.tmdb.org/t/p/w185${movieResults[i].poster_path}?api_key=1ef33d0988889fd4f6c374211d20e38c`;
+      newImg.alt = movieResults[i].title;
+
+      let newText = document.createElement("h3");
+
+      newText.textContent = movieResults[i].title;
+
+      newDiv.appendChild(newImg);
+      newDiv.appendChild(newText);
+
+      movieContainer.appendChild(newDiv);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+})();
+
+// immediately invoked functions IIFE
+
+
+
     
-   
-
-
 let myname: string = 'chioma';
 let score: number = 100;
-
-
 
 //Two ways of assinging Typescritp
 //1. inference;     let TypeScript infer the type of a variable based on its value
